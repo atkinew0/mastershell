510d1b431b601f66ae24e41a223dfa80ff506b81
Eli Atkinson <atkinson.eli@gmail.com>
2019-01-25 13:54:41 -0500

    fixed bug in signout

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -4,10 +4,16 @@ import * as actions from '../actions'
 
 
 class Signout extends Component{
+    constructor(props){
+        super(props)
+        this.state = {};
+    }
 
     componentDidMount(){
 
         this.props.signout();
+        console.log("state now",this.props.mySocket)
+        this.props.mySocket.websocket.close()
     }
 
     render(){
@@ -18,7 +24,7 @@ class Signout extends Component{
 function mapStateToProps(state){
     
     console.log("State of now",state)
-    return { websocket: state.websocket}
+    return { mySocket : state.websocket}
 }
 
 
